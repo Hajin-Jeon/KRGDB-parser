0b483c2096b3e4a4cccb5df08b893c235da2c20e
Hajin-Jeon <hajinjeon@kribb.re.kr>
2024-04-02 15:46:00 +0900

    supporting for merged or unsupported data

--- krgdb_parser.py
+++ krgdb_parser.py
@@ -31,6 +31,12 @@ def parse_html(rs_id, html_doc, output_file=None):
     soup = BeautifulSoup(html_doc, 'html.parser')
     results = []
 
+    position_dt = soup.find('dt', string="Position")
+    position_dd = position_dt.find_next_sibling('dd')
+
+    position_info = position_dd.find('span').text.strip()
+    grch_version = position_dd.find('span', string=re.compile(r'GRCh')).text.strip()
+
     for tr in soup.find_all('tr'):
         a_tags = tr.find_all('a')
         for a in a_tags:
@@ -48,14 +54,35 @@ def parse_html(rs_id, html_doc, output_file=None):
                     ref, ref_freq = frequencies[0]
                     alts = frequencies[1:]
                     alt_str = '\t'.join([f"{allele}\t{freq}" for allele, freq in alts])
-                    result_line = f"{rs_id}\t{sample_size}\t{ref}\t{ref_freq}\t{alt_str}"
+                    result_line = f"{rs_id}\t{position_info}\t{grch_version}\t{sample_size}\t{ref}\t{ref_freq}\t{alt_str}"
                     results.append(result_line)
     
+    if not results:
+        results.append(f"{rs_id}\t{position_info}\t{grch_version}\t0")
+
     if output_file:
         with open(output_file, 'a') as file:
             file.writelines('\n'.join(results) + '\n')
     else:
         print('\n'.join(results))
+
+def check_merge(html_content, rs_id):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    status_dt = soup.find('dt', string="Status")
+    if status_dt:
+        status_dd = status_dt.find_next_sibling('dd')
+        if status_dd and 'was merged into' in status_dd.text:
+            new_rs_id_match = re.search(r'href="([^"]+)"', str(status_dd))
+            if new_rs_id_match:
+                new_rs_id = new_rs_id_match.group(1).split('/')[-1]
+                print(f"{rs_id} was merged into {new_rs_id}. Fetching new SNP.")
+                return new_rs_id
+        if status_dd and 'Unsupported.' in status_dd.text:
+            print(f"{rs_id} no longer any supporting observations.")
+            return 0
+    return rs_id
+
 
 def process_single_rs(rs_id, output_file=None):
     if not os.path.exists(rs_id):
@@ -64,7 +91,17 @@ def process_single_rs(rs_id, output_file=None):
         with open(rs_id, 'r', encoding='utf-8') as file:
             html_content = file.read()
     if html_content:
-        parse_html(rs_id, html_content, output_file)
+        new_rs = check_merge(html_content, rs_id)
+        if new_rs == rs_id:
+            parse_html(rs_id, html_content, output_file)
+        elif new_rs == 0:
+            if output_file:
+                with open(output_file, 'a') as file:
+                    file.writelines(f"{rs_id}\tUnsupported" + '\n')
+            else:
+                print(f"{rs_id}\tUnsupported")
+        else:
+            process_single_rs(new_rs, output_file)
 
 def process_rs_file(input_arg, output_file=None):
     with open(input_arg, 'r', encoding='utf-8') as file:
